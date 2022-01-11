c1326a349ea843d92f709b1640a534b163d8db3b
Ajab Tomar <rahultomar007@outlook.com>
2022-01-12 02:32:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg'
+import {Button} from 'react-bootstrap'
+function App(){
+  return(
+    <div className='App'>
+      <h1>E-com project</h1>
+      <button>Normal Button</button>
+      <Button>bootstrap button</Button>
+    </div>
+  )
+}
+export default App;
